f67a2a0e6fca8f249480e813d5abd80210e831c7
HuyNguyen <quanghnguyen@linagora.com>
2025-05-26 10:48:56 +0700

    fixup! fixup! Add recent emoji tab

--- lib/src/data/emoji_data.dart
+++ lib/src/data/emoji_data.dart
@@ -77,6 +77,26 @@ class EmojiData with _$EmojiData {
     return emoji.skins.first.native;
   }
 
+  String getIdByEmoji(
+    String emoji, {
+    EmojiSkinTone skinTone = EmojiSkinTone.none,
+  }) {
+    final emojiEntry = emojis.entries.firstWhere(
+      (entry) => entry.value.skins.any(
+        (skin) => skin.native == emoji,
+      ),
+      orElse: () =>
+          throw ArgumentError.value(emoji, 'emoji', 'Emoji not found'),
+    );
+    final emojiId = emojiEntry.key;
+    final emojiData = emojiEntry.value;
+
+    if (0 <= skinTone.index && skinTone.index < emojiData.skins.length) {
+      return emojiId;
+    }
+    return emojiId; // Return the first skin's id
+  }
+
   EmojiData filterByKeyword(
     String keyword, {
     bool keepEmptyCategories = false,
